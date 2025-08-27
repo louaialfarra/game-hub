47e5a7a040d9d1e488d914f19293c78df7a48439
louaialfarra <156582409+louaialfarra@users.noreply.github.com>
2025-08-27 23:17:53 +0300

    test dark
    dark

--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -13,18 +13,18 @@ import { ColorModeProvider, type ColorModeProviderProps } from "./color-mode";
 //   theme: {
 //     tokens: {
 //       colors: {
-//         gray: {
-//           50: { value: "#f2f2f2" },
-//           100: { value: "#d9d9d9" },
-//           200: { value: "#bfbfbf" },
-//           300: { value: "#a6a6a6" },
-//           400: { value: "#8c8c8c" },
-//           500: { value: "#737373" },
-//           600: { value: "#595959" },
-//           700: { value: "#404040" },
-//           800: { value: "#262626" },
-//           900: { value: "#0d0d0d" },
-//         },
+//         // gray: {
+//         //   50: { value: "#f2f2f2" },
+//         //   100: { value: "#d9d9d9" },
+//         //   200: { value: "#bfbfbf" },
+//         //   300: { value: "#a6a6a6" },
+//         //   400: { value: "#8c8c8c" },
+//         //   500: { value: "#737373" },
+//         //   600: { value: "#595959" },
+//         //   700: { value: "#404040" },
+//         //   800: { value: "#262626" },
+//         //   900: { value: "#0d0d0d" },
+//         // },
 //       },
 //     },
 //   },
@@ -34,7 +34,7 @@ import { ColorModeProvider, type ColorModeProviderProps } from "./color-mode";
 export function Provider(props: ColorModeProviderProps) {
   return (
     <ChakraProvider value={defaultSystem}>
-      <ColorModeProvider {...props} />
+      <ColorModeProvider {...props} forcedTheme="dark" />
     </ChakraProvider>
   );
 }
